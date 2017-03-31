15006e18b84f0e42be6811a761d31fba4b806589
VladyslavBiletskyi <vladyslav.biletskyi@nure.ua>
2017-03-31 23:34:46 +0300

    Setup Solution

--- MVCClient/Controllers/HomeController.cs
+++ MVCClient/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace MVCClient.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "E-Register system";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contacts";
 
             return View();
         }
